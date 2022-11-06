328054ee5083e5ccb145299420db246f33eaffcb
sanjay prajapati <70086773+i-sanjay-cs@users.noreply.github.com>
2022-11-06 18:53:06 +0530

    Add files via upload

---
+++ character-recognition.py
@@ -0,0 +1,16 @@
+import cv2
+import pytesseract
+import numpy as np
+from PIL import ImageGrab
+import time
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+img = cv2.imread('sign1.png')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+pytesseract
+##############################################
+##### Image to String   ######
+##############################################
+print(pytesseract.image_to_string(img))
+cv2.imshow('img', img)
+cv2.waitKey(0)
